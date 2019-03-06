594ef59251a3c7ec709f40c14caa6cb47fae34ed
Michael Bouhy <mibouhy@microsoft.com>
2019-03-06 17:48:03 +0200

    switch instead of if

--- LoadGenerator/SendMessageClient.cs
+++ LoadGenerator/SendMessageClient.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,117 +22,94 @@ namespace LoadGenerator
 
             _clientType = clientType;
 
-            if (ClientType.QueueClient == clientType)
+            switch (_clientType)
             {
-                _queueClient = QueueClient.CreateFromConnectionString(connectionString, eHOrQueueOrTopicName);
-                return;
+                case ClientType.QueueClient:
+                    _queueClient = QueueClient.CreateFromConnectionString(connectionString, eHOrQueueOrTopicName);
+                    return;
+                case ClientType.EventHub:
+                    var mf = MessagingFactory.CreateFromConnectionString(connectionString + ";TransportType=Amqp" + ";OperationTimeout=00:00:02");
+                    _eventHubClient = mf.CreateEventHubClient(eHOrQueueOrTopicName);
+                    return;
+                case ClientType.CloudQueueClient:
+                    var storageAccount = CloudStorageAccount.Parse(connectionString);
+                    var queueClient = storageAccount.CreateCloudQueueClient();
+                    var queue = queueClient.GetQueueReference(eHOrQueueOrTopicName);
+                    queue.CreateIfNotExists();
+                    _cloudQueue = queue;
+                    return;
+                default:
+                    throw new NotImplementedException();
             }
-
-            if (ClientType.EventHub == clientType)
-            {
-                var mf = MessagingFactory.CreateFromConnectionString(connectionString + ";TransportType=Amqp" + ";OperationTimeout=00:00:02");
-                _eventHubClient = mf.CreateEventHubClient(eHOrQueueOrTopicName);
-                return;
-            }
-
-            if (ClientType.CloudQueueClient == clientType)
-            {
-                var storageAccount = CloudStorageAccount.Parse(connectionString);
-                var queueClient = storageAccount.CreateCloudQueueClient();
-                var queue = queueClient.GetQueueReference(eHOrQueueOrTopicName);
-                queue.CreateIfNotExists();
-                _cloudQueue = queue;
-                return;
-            }
-
-            throw new NotImplementedException();
         }
 
         public Task SendAsync(string payload)
         {
-            if (ClientType.QueueClient == _clientType)
+            switch (_clientType)
             {
-                var message = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(payload)))
-                {
-                    ContentType = "application/json",
-                    Label = "MyPayload",
-                    TimeToLive = TimeSpan.FromMinutes(100)
-                };
-                return _queueClient.SendAsync(message);
+                case ClientType.QueueClient:
+                    var message = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(payload)))
+                    {
+                        ContentType = "application/json",
+                        Label = "MyPayload",
+                        TimeToLive = TimeSpan.FromMinutes(100)
+                    };
+                    return _queueClient.SendAsync(message);
+                case ClientType.EventHub:
+                    return _eventHubClient.SendAsync(new EventData(new MemoryStream(Encoding.UTF8.GetBytes(payload))));
+                case ClientType.CloudQueueClient:
+                    var options = new QueueRequestOptions() { ServerTimeout = TimeSpan.FromSeconds(1), MaximumExecutionTime = TimeSpan.FromSeconds(2) };
+                    return _cloudQueue.AddMessageAsync(new CloudQueueMessage(payload), null, null, options, null);
+                default:
+                    throw new NotImplementedException();
             }
-
-            if (ClientType.EventHub == _clientType)
-            {
-                var message = new EventData(new MemoryStream(Encoding.UTF8.GetBytes(payload)));
-                return _eventHubClient.SendAsync(message);
-            }
-
-            if (ClientType.CloudQueueClient == _clientType)
-            {
-                CloudQueueMessage message = new CloudQueueMessage(payload);
-                var options = new QueueRequestOptions() { ServerTimeout = TimeSpan.FromSeconds(1), MaximumExecutionTime = TimeSpan.FromSeconds(2) };
-                return _cloudQueue.AddMessageAsync(message, null, null, options, null);
-            }
-
-            throw new NotImplementedException();
         }
 
         public Task SendBatchAsync(List<string> payloads)
         {
-            if (ClientType.QueueClient == _clientType)
+            switch (_clientType)
             {
-                var messageBatch = new List<BrokeredMessage>();
-                payloads.ForEach(payload => messageBatch.Add(new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(payload)))
-                {
-                    ContentType = "application/json",
-                    Label = "MyPayload",
-                    TimeToLive = TimeSpan.FromMinutes(100)
-                }));
+                case ClientType.QueueClient:
+                    var messageBatchBrokeredMessage = new List<BrokeredMessage>();
+                    payloads.ForEach(payload => messageBatchBrokeredMessage.Add(new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(payload)))
+                    {
+                        ContentType = "application/json",
+                        Label = "MyPayload",
+                        TimeToLive = TimeSpan.FromMinutes(100)
+                    }));
 
-                return _queueClient.SendBatchAsync(messageBatch);
+                    return _queueClient.SendBatchAsync(messageBatchBrokeredMessage);
+                case ClientType.EventHub:
+                    var messageBatchEventHub = new List<EventData>();
+                    payloads.ForEach(payload => messageBatchEventHub.Add(new EventData(new MemoryStream(Encoding.UTF8.GetBytes(payload)))));
+                    return _eventHubClient.SendBatchAsync(messageBatchEventHub);
+                case ClientType.CloudQueueClient:
+                    var bigPayload = $"[{string.Join(",", payloads)}]";
+                    var joinedPayload = Encoding.UTF8.GetBytes(bigPayload);
+                    var messageBatchCloudQueueMessage = new CloudQueueMessage(joinedPayload);
+                    var options = new QueueRequestOptions() { ServerTimeout = TimeSpan.FromSeconds(1), MaximumExecutionTime = TimeSpan.FromSeconds(2) };
+                    return _cloudQueue.AddMessageAsync(messageBatchCloudQueueMessage, null, null, options, null);
+                default:
+                    throw new NotImplementedException();
             }
-
-            if (ClientType.EventHub == _clientType)
-            {
-                var aggregationLag = Stopwatch.StartNew();
-                var messageBatch = new List<EventData>();
-                payloads.ForEach(payload => messageBatch.Add(new EventData(new MemoryStream(Encoding.UTF8.GetBytes(payload)))));
-                return _eventHubClient.SendBatchAsync(messageBatch);
-            }
-
-            if (ClientType.CloudQueueClient == _clientType)
-            {
-                var bigPayload = string.Join(",", payloads);
-                var joinedPayload = Encoding.UTF8.GetBytes(bigPayload);
-                var messageBatch = new CloudQueueMessage(joinedPayload);
-                var options = new QueueRequestOptions() { ServerTimeout = TimeSpan.FromSeconds(1), MaximumExecutionTime = TimeSpan.FromSeconds(2) };
-
-                return _cloudQueue.AddMessageAsync(messageBatch, null, null, options, null);
-            }
-
-            throw new NotImplementedException();
         }
 
         public Task CloseAsync()
         {
-            if (ClientType.QueueClient == _clientType)
+            switch (_clientType)
             {
-                return _queueClient.CloseAsync();
+                case ClientType.QueueClient:
+                    return _queueClient.CloseAsync();
+                case ClientType.EventHub:
+                    return _eventHubClient.CloseAsync();
+                case ClientType.CloudQueueClient:
+                    return Task.CompletedTask;
+                default:
+                    throw new NotImplementedException();
             }
-
-            if (ClientType.EventHub == _clientType)
-            {
-                return _eventHubClient.CloseAsync();
-            }
-
-            if (ClientType.CloudQueueClient == _clientType)
-            {
-                return Task.CompletedTask;
-            }
-
-            throw new NotImplementedException();
         }
     }
+
     public enum ClientType
     {
         Undefined = 0,
